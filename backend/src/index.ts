b7db626584837ef10974cfb795e9866031d8e069
vincebax <vbaxley1107@gmail.com>
2025-07-27 22:55:40 -0500

    Refactor API structure

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,9 @@ import express from 'express';
 import { createClient } from '@supabase/supabase-js';
 import {  v4 as uuidv4, validate as uuidValidate } from 'uuid';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import fs from 'node:fs';
+import path from 'node:path'
 
 dotenv.config();
 
@@ -14,56 +17,68 @@ if (!supabaseUrl || !serviceKey) {
 
 const supabase = createClient(supabaseUrl, serviceKey);
 
-const app = express()
-app.use(express.json())
-const port = 3000
+const app = express();
+app.use(express.json());
+app.use(cors());
+const port = 3000;
 
 type UUID = string;
 
-interface IdJson {
-    id: UUID
-}
-
-interface AddNodeJson {
+interface AddNodeRequestBody {
     origin: UUID
     title: string
 };
 
-interface UpdateNodeJson {
-    id: UUID,
+interface UpdateNodeRequestBody {
     title: string,
-    contents: string
+    bufferBase64: string
 }
 
-app.get('/nodes', async (req, res) => {
+interface Node {
+    id: UUID,
+    title: string,
+    file_link: string
+}
+
+interface Edge {
+    id: UUID,
+    source: UUID,
+    target: UUID
+}
+
+interface MarkdownFile {
+    id: UUID,
+    file_link: string
+}
+
+app.get('/nodes/:id', async (req, res) => {
 
     /**
      * 
      * endpoint to fetch a node from the local database
      * 
-     * params (request body):
+     * params (url):
      * id - uuid corresponding to the node of interest
      *
      * returns:
      * object corresponding to the node data
      *  
      */
-
-    const recievedJson: IdJson = req.body;
     
-    if (recievedJson && typeof recievedJson === 'object' && uuidValidate(recievedJson.id)) {
+    if (uuidValidate(req.params.id)) {
         const { data, error } = await supabase
         .from('Nodes')
-        .select('id, title, contents')
-        .eq('id', recievedJson.id)
+        .select('id, title, file_link')
+        .eq('id', req.params.id)
 
         if (error) {
             return res.status(500).send(`Supabase error: ${error.code} - ${error.message}`);
         }
 
         res.json(data[0]); // [0] because there should only ever be one entry with a given uuid
+
     } else {
-        if (!uuidValidate(recievedJson.id)) {
+        if (!uuidValidate(req.params.id)) {
             return res.status(400).send('Invalid UUID passed as node ID');
         }
         return res.status(400).send('Invalid data format');
@@ -86,24 +101,55 @@ app.post('/nodes', async (req, res) => {
      *  
      */
 
-    const recievedJson: AddNodeJson = req.body;
+    const newNode: AddNodeRequestBody = req.body;
     
-    if (recievedJson && typeof recievedJson === 'object' && recievedJson.title) {
+    if (newNode && typeof newNode === 'object' && newNode.title) {
+
         const newNodeId = uuidv4();
 
+
+        // insert new empty markdown file with appropriate name to the markdown-data storage bucket
+
+        // create a temporary empty markdown file to insert
+        const tempFilePath = path.join(__dirname, '/temp.md');
+
+        await fs.writeFile(tempFilePath, '', err => {
+            
+        if (err) {
+            console.error(`Error creating temporary markdown file ${err}`);
+        } else {
+            console.log('Temporary file written successfully');
+        }
+        });
+
+        // dump the temporary file to the supabase bucket with correct naming
+        const newMarkdownFileId = uuidv4();
+        const file_link = await emptyFileToBucket(newMarkdownFileId, tempFilePath, 'markdown-files');
+
+        // delete temporary file that was written
+
+        await fs.unlink(tempFilePath, (err) => {
+        if (err) {
+            console.error('Error deleting file:', err);
+        } else {
+            console.log('Temporary file deleted successfully!');
+        }
+        });
+
+        // insert given data to the nodes database
         const { error } = await supabase
         .from('Nodes')
-        .insert({ id: newNodeId, title: recievedJson.title });
+        .insert({ id: newNodeId, title: newNode.title, file_link: file_link });
 
         if (error) {
             return res.status(500).send(`Supabase error: ${error.code} - ${error.message}`);
         }
 
-        if (recievedJson.origin) {
+        if (newNode.origin) {
             const newEdgeId = uuidv4();
             const { error } = await supabase
-            .from('Edges')
-            .insert({ id: newEdgeId, origin: recievedJson.origin, destination: newNodeId });
+            .from('Links')
+            .insert({ id: newEdgeId, source: newNode.origin, target: newNodeId });
 
             if (error) {
                 return res.status(500).send(`Supabase error: ${error.code} - ${error.message}`);
@@ -117,36 +163,52 @@ app.post('/nodes', async (req, res) => {
 
 });
 
-app.patch('/nodes', async (req, res) => {
+app.patch('/nodes/:id', async (req, res) => {
 
     /**
      * 
-     * endpoint to insert nodes into the local database
+     * endpoint to update node in the local database
      * 
      * params (request body):
-     * id - uuid corresponding to the node of interest
+     * title - new title you want the node to have
+     * bufferBase64 - base64 encoded string that represents the new version of the file you want to update storage to
      *
      * returns:
      * nothing
      *  
      */
     
-    const recievedJson: UpdateNodeJson = req.body;
+    const newNodeData: UpdateNodeRequestBody = req.body;
 
-    if (recievedJson && typeof recievedJson === 'object' && uuidValidate(recievedJson.id)) {
-        const { error } = await supabase
-        .from('Nodes')
-        .update({ 'title': recievedJson.title, 'contents': recievedJson.contents })
-        .eq('id', recievedJson.id);
+    if (newNodeData && typeof newNodeData === 'object' && uuidValidate(req.params.id)) {
 
-        if (error) {
-            return res.status(500).send(`Supabase error: ${error.code} - ${error.message}`);
+        // update title if it needs updating
+
+        if (newNodeData.title) {
+            const { error } = await supabase
+            .from('Nodes')
+            .update({ 'title': newNodeData.title })
+            .eq('id', req.params.id);
+
+            if (error) {
+                return res.status(500).send(`Supabase error: ${error.code} - ${error.message}`);
+            }
+        }
+
+        // update the associated bucket storage if there is a new file version provided
+
+        if (newNodeData.bufferBase64) {
+            
+            // make a buffer from the base64 encoded string passed
+            const fileBuffer = Buffer.from(newNodeData.bufferBase64, 'base64');
+
+            await givenFileToBucket(req.params.id, fileBuffer, 'markdown-files');
         }
 
         return res.send("Successful update");
 
     } else {
-        if (!uuidValidate(recievedJson.id)) {
+        if (!uuidValidate(req.params.id)) {
             return res.status(400).send('Invalid UUID passed as node ID');
         }
         return res.status(400).send('Invalid data format');
@@ -154,13 +216,13 @@ app.patch('/nodes', async (req, res) => {
     
 });
 
-app.delete('/nodes', async (req, res) => {
+app.delete('/nodes/:id', async (req, res) => {
 
     /**
      * 
      * endpoint to delete nodes from the local database
      * 
-     * params (request body):
+     * params (url):
      * id - uuid corresponding to the node of interest
      *
      * returns:
@@ -168,13 +230,31 @@ app.delete('/nodes', async (req, res) => {
      *  
      */
 
-    const recievedJson: IdJson = req.body;
+    if (uuidValidate(req.params.id)) {
 
-    if (recievedJson && typeof recievedJson === 'object' && uuidValidate(recievedJson.id)) {
+        // get a reference to the target node
+
+        const { data, error } = await supabase
+        .from('Nodes')
+        .select('id, title, file_link')
+        .eq('id', req.params.id)
+
+        if (error) {
+            return res.status(500).send(`Supabase error: ${error.code} - ${error.message}`);
+        }
+
+        const target = data[0];
+
+        // delete corresponding entry in the data bucket (no check since one is created on every addition)
+
+        await deleteGivenBucketEntry(target.file_link, 'markdown-files');
+
+        // delete entry from the Nodes table
+        
         const response = await supabase
         .from('Nodes')
         .delete()
-        .eq('id', recievedJson.id)
+        .eq('id', req.params.id)
 
         if (response.error) {
             const error = response.error;
@@ -182,8 +262,9 @@ app.delete('/nodes', async (req, res) => {
         };
 
         return res.send('Successful deletion');
+
     } else {
-        if (!uuidValidate(recievedJson.id)) {
+        if (!uuidValidate(req.params.id)) {
             return res.status(400).send('Invalid UUID passed as node ID');
         }
         return res.status(400).send('Invalid data format');
@@ -191,7 +272,7 @@ app.delete('/nodes', async (req, res) => {
 
 });
 
-app.get('/edges', async (req, res) => {
+app.get('/links/:id', async (req, res) => {
     /**
      * 
      * endpoint to fetch edge data from the local database
@@ -203,14 +284,12 @@ app.get('/edges', async (req, res) => {
      * nothing
      *  
      */
-
-    const recievedJson: IdJson = req.body;
     
-    if (recievedJson && typeof recievedJson === 'object' && uuidValidate(recievedJson.id)) {
+    if (uuidValidate(req.params.id)) {
         const { data, error } = await supabase
-        .from('Edges')
-        .select('id, origin, destination')
-        .eq('id', recievedJson.id)
+        .from('Links')
+        .select('id, source, target')
+        .eq('id', req.params.id)
 
         if (error) {
             return res.status(500).send(`Supabase error: ${error.code} - ${error.message}`);
@@ -218,13 +297,86 @@ app.get('/edges', async (req, res) => {
 
         res.json(data[0]); // [0] because there should only ever be one entry with a given uuid
     } else {
-        if (!uuidValidate(recievedJson.id)) {
+        if (!uuidValidate(req.params.id)) {
             return res.status(400).send('Invalid UUID passed as node ID');
         }
         return res.status(400).send('Invalid data format');
     }
 });
 
+app.get('/test', (req, res) => {
+    return res.send('hello');
+});
+
 app.listen(port, () => {
     console.log(`app running on port ${port}`);
 });
+
+async function emptyFileToBucket(id: UUID, filePath: string, bucketName: string) {
+
+    try {
+
+        const tempFileBuffer = await fs.promises.readFile(filePath);
+
+        const { data, error } = await supabase
+        .storage
+        .from(bucketName)
+        .upload(`${id}/data.md`, tempFileBuffer, {
+            contentType: 'text/markdown',
+        });
+
+        if (error) {
+            console.log('Error uploading image to bucket');
+        }
+
+        return data?.path;
+
+    } catch (err) {
+        console.log('Error reading file or uploading: ', err);
+        return null;
+    }
+
+}
+
+async function givenFileToBucket(id: UUID, fileBuffer: Buffer, bucketName: string) {
+
+    try {
+
+        const { data, error } = await supabase
+        .storage
+        .from(bucketName)
+        .upload(`${id}/data.md`, fileBuffer, {
+            contentType: 'text/markdown',
+        });
+
+        if (error) {
+            console.log('Supabase error uploading image to bucket');
+        }
+
+        return data?.path;
+
+    } catch (err) {
+        console.log('Error reading file or uploading: ', err);
+        return null;
+    }
+
+}
+
+async function deleteGivenBucketEntry(file_link: string, bucketName: string) {
+
+    try {
+        
+        const { data, error } = await supabase
+        .storage
+        .from(bucketName)
+        .remove([file_link]);
+
+        if (error) {
+            console.log("Supabase error deleting data from bucket");
+        }
+
+    } catch (err) {
+        console.log('Error deleting bucket data', err);
+    }
+
+}
---
+++ backend/supabase/migrations/20250727051130_initial_schema.sql
@@ -0,0 +1,7 @@
+alter table "public"."Files" disable row level security;
+
+alter table "public"."Links" disable row level security;
+
+alter table "public"."Nodes" disable row level security;
+
+
